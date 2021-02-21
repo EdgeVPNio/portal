31f0b77932ceb650d3056748b71858a5ebe0a351
RajathUbuntu <rajath2774@gmail.com>
2021-02-21 18:49:26 -0500

    Added the package dependencies and modified the setup to use it.

--- setup.sh
+++ setup.sh
@@ -2,18 +2,13 @@
 
 curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt-get install -y nodejs
-npm init
-npm install express --save
+npm install
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
-npm install mongodb --save
-npm install mongoose --save
-npm install --save dotenv
-npm install --save --legacy-peer-deps google-map-react
 #commenting out required UI(React)dependencies but already added to package.json in older versions
 #npm install --save react
 #npm install --save react-dom
