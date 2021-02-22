8c1c1801b5e765f9dc7dc3398efb24f2be5d84d5
Prajwala <56329359+PrajwalaNagaraj@users.noreply.github.com>
2021-02-22 11:49:14 -0500

    Reverting setup.sh for changes without docker.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-curl -sL https://deb.nodesource.com/setup_15.x | bash -
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 apt-get install -y nodejs
-apt-get install -y systemd
 npm install --legacy-peer-deps
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 apt-get update
 apt-get install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
-#commenting out required UI(React)dependencies but already added to package.json in older versions
-#npm install --save react
-#npm install --save react-dom
-#npm install bootstrap
-#npm install react-router-dom
-#npm install axios
-#npm install react-bootstrap
-#npm install react-bootstrap-typeahead
-#npm install react-cytoscapejs
-#npm install react-tippy
+cd views && npm run build
