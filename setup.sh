55fca829c198a2b99629a2d043ebcaada0eb1829
Prajwalanagaraj <prajwalanagraj@gmail.com>
2021-02-22 10:04:54 -0500

    changing setup.sh wihtout sudo for dockerfile creation

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_15.x | -E bash -
+apt-get install -y nodejs
 npm install --legacy-peer-deps
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+apt-get update
+apt-get install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
 #commenting out required UI(React)dependencies but already added to package.json in older versions
 #npm install --save react
 #npm install --save react-dom
