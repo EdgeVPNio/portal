6e2aaf40adb4d6b511072cc1381b0598cf9a8cee
Prajwalanagaraj <prajwalanagraj@gmail.com>
2021-02-22 10:01:05 -0500

    Changes to accomodate updated dependencies wrt react, setup.sh and server.js

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt-get install -y nodejs
-npm install
+npm install --legacy-peer-deps
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
