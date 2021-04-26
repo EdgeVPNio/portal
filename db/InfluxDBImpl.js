1d77402fbcf5a5024a9b541fbdc38ea45ec20056
RajathUbuntu <rajath2774@gmail.com>
2021-04-26 08:59:25 -0400

    Added getOverlays method

--- db/InfluxDBImpl.js
+++ db/InfluxDBImpl.js
@@ -158,6 +158,57 @@ class InfluxDBImpl extends DataBaseInterface {
         * @param {String} overlayId 
         * @returns inserted data to topology collection
         */
+         async getOverlays(tableName, intervalId) {
+                var that = this;
+                var overlaysData = null;
+                async function streamReady() {
+                        return new Promise(ok => {
+                                var overlaysInterval = setInterval(function () {
+                                        //console.log("Data at setInterval is ", overlaysData);
+                                        if (overlaysData) {
+                                                clearInterval(overlaysInterval)
+                                                return ok();
+                                        } else {
+                                                overlaysData = that.db.query('select _id from ${tableName} WHERE _id > ${intervalId} ORDER BY time DESC LIMIT 1')
+                                                        .then(intervalId => {
+                                                                if (intervalId) {
+                                                                        return that.db.query('select Overlays from ${tableName} ORDER BY time DESC LIMIT 1');
+                                                                } else {
+                                                                        return null;
+                                                                }
+                                                        });
+                                        }
+                                }, 1000);
+                        });
+                }
+                this.findOverlays(tableName, intervalId)
+                        .then(data => {
+                                if (Object.keys(data).length === 0) {
+                                        console.log("No data found, setting data to null.")
+                                        overlaysData = null;
+                                        //setinterval to check the topology table for new entry every 3 sec
+                                        //querying only for intervalId
+                                } else {
+                                        //console.log("Got data from DB not waitng. Data:", data);
+                                        overlaysData = data;
+                                }
+                        })
+                var newData = await streamReady()
+                        .then(data => {
+                                //console.log("Inside stream Ready ", data);
+                                return data;
+                        });
+                //console.log("End of topology DB call with ", newData, topologyData)
+                return overlaysData;
+        }
+
+        /**
+        * Function to query Topology collection, watch the collection for insert op every 1 second
+        * @param {String} tableName 
+        * @param {Float} intervalId 
+        * @param {String} overlayId 
+        * @returns inserted data to topology collection
+        */
         async getTopology(tableName, intervalId, overlayId) {
                 var that = this;
                 var topologyData = null;
@@ -172,7 +223,7 @@ class InfluxDBImpl extends DataBaseInterface {
                                                 topologyData = that.db.query('select _id from ${tableName} WHERE _id > ${intervalId} AND OverlayId=${overlayId} ORDER BY time DESC LIMIT 1')
                                                         .then(intervalId => {
                                                                 if (intervalId) {
-                                                                        return that.db.query('select _id from ${tableName} WHERE OverlayId=${overlayId} ORDER BY time DESC LIMIT 1');
+                                                                        return that.db.query('select Topology from ${tableName} WHERE OverlayId=${overlayId} ORDER BY time DESC LIMIT 1');
                                                                 } else {
                                                                         return null;
                                                                 }
@@ -194,7 +245,7 @@ class InfluxDBImpl extends DataBaseInterface {
                                         topologyData = data;
                                 }
                         })
-                var newData = await streamReady(tableName, overlayId)
+                var newData = await streamReady()
                         .then(data => {
                                 //console.log("Inside stream Ready ", data);
                                 return data;
