efc6a831a00af2cbda9bc0a8a6a338cb8b1e4b7e
Dennis-Gireesh <dennis.gireesh@gmail.com>
2021-08-11 17:46:28 -0400

    Added subgraphcyelements prop

--- src/features/evio/evioSlice.js
+++ src/features/evio/evioSlice.js
@@ -1,3 +1,24 @@
+/* EdgeVPNio
+ * Copyright 2021, University of Florida
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 import { createSlice } from "@reduxjs/toolkit";
 import { 
   elementTypes, 
@@ -9,6 +30,7 @@ const evioSlice = createSlice({
   initialState: {
     selectedOverlayId: "",
     cyElements: [],
+    subgraphCyElements: [],
     selectedElementType: elementTypes.eleTypeNone,
     selectedCyElementData: JSON.stringify({}),
     redrawGraph: false,
@@ -19,6 +41,9 @@ const evioSlice = createSlice({
     },
     setCyElements(state, action) {
       state.cyElements = action.payload;
+    },
+    setSubgraphCyElements(state, action) {
+      state.subgraphCyElements = action.payload;
     },
     setSelectedElement(state, action) {
       state.selectedElementType = action.payload.selectedElementType;
@@ -39,6 +64,7 @@ const evioSlice = createSlice({
 export const {
   setSelectedOverlayId,
   setCyElements,
+  setSubgraphCyElements,
   setSelectedElement,
   clearSelectedElement,
   setRedrawGraph,
