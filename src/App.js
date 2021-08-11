a46adf5050db3dc7b6e3898bfaf5336f10ff5ef6
Dennis-Gireesh <dennis.gireesh@gmail.com>
2021-08-11 17:41:26 -0400

    Added mapview

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
+/* EdgeVPNio
+ * Copyright 2021, University of Florida
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 import React from "react";
 import "react-tippy/dist/tippy.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,6 +29,7 @@ import { setSelectedView } from "./features/view/viewSlice";
 import OverlaysView from "./app/OverlaysView";
 import TopologyView from "./app/TopologyView";
 import Toolbar from "./app/Toolbar";
+import MapView from "./app/MapView";
 import Breadcrumb from "./app/Breadcrumb";
 
 class App extends React.Component {
@@ -23,7 +45,9 @@ class App extends React.Component {
       case "TopologyView":
         return <TopologyView />;
       case "SubgraphView":
-          return <TopologyView />;
+        return <TopologyView />;
+      case "MapView":
+        return <MapView />;
       default:
         return <Spinner id="loading" animation="border" variant="info" />;
     }
